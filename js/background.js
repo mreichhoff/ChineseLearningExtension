bfeeb7e814a6b341e90fc4375f8056aea4f2c0b2
Matthew Reichhoff <matthew.reichhoff@gmail.com>
2024-11-24 12:46:21 -0500

    Add initial popover styling, use CSS Anchor API

--- js/background.js
+++ js/background.js
@@ -2,9 +2,22 @@ import { definitions } from "../data/definitions.js";
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
         sendResponse({ definitions: definitions[request.word] });
     }
 );
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    // The callback for runtime.onMessage must return falsy if we're not sending a response
+    (async () => {
+        console.log(message);
+        if (message.type === 'open_side_panel') {
+            // This will open a tab-specific side panel only on the current tab.
+            await chrome.sidePanel.open({ tabId: sender.tab.id });
+            await chrome.sidePanel.setOptions({
+                tabId: sender.tab.id,
+                path: 'sidepanel-tab.html',
+                enabled: true
+            });
+        }
+    })();
+});
--- js/content.js
+++ js/content.js
@@ -1,4 +1,5 @@
 import { pinyin } from "../data/definitions.js";
+import { html, render } from 'lit-html';
 
 let modifications = [];
 
@@ -18,9 +19,117 @@ function processNode(node) {
     }
 }
 
+function parseDefinitions(definitionList) {
+    let parsedDefinitions = {};
+    for (const item of definitionList) {
+        // TODO: I goofed...
+        // definitions format should be:
+        // key: word (or character)
+        // val: [{pinyin: a, definitions: [a,b,c], measure: [a,b,c]}...]
+        // this would better match cedict's format
+        // could consider combining simplified and trad, but separating for
+        // the purpose of sentences seems better
+        // should also verify this format...
+        const key = item.pinyin;
+        if (!(key in parsedDefinitions)) {
+            parsedDefinitions[key] = [item];
+        } else {
+            parsedDefinitions[key].push(item);
+        }
+    }
+    return parsedDefinitions;
+}
+
 const popover = document.createElement('div');
+popover.classList.add('chineselearningextension-popover');
 popover.popover = 'auto';
 document.body.appendChild(popover);
+
+function getToneColor(tone) {
+    return tone === '1' ? '#eb3434' : tone === '2' ? 'green' : tone === '3' ? '#8f34eb' : tone === '4' ? '#345eeb' : '#333';
+}
+function renderPinyin(pinyin) {
+    const pinyinTemplates = [];
+    const syllables = pinyin.split(' ');
+    syllables.forEach(syllable => {
+        pinyinTemplates.push(
+            html`<span class="chineselearningextension-definition-pinyin" style="color:${getToneColor(syllable[syllable.length - 1])}">
+                ${syllable}
+            </span>`
+        );
+    });
+    return pinyinTemplates;
+}
+
+function renderDefinitions(definitions) {
+    const definitionTemplates = [];
+    definitions.forEach((definition, index) => {
+        definitionTemplates.push(
+            html`<span>
+                <span class="chineselearningextension-definition-number">${index + 1}:</span>
+                <span>${definition.en}</span>
+            </span>`
+        );
+    });
+    return definitionTemplates;
+}
+
+function renderDictionary(word, parsedDefinitions, container) {
+    const itemTemplates = [];
+    Object.entries(parsedDefinitions).forEach(([pinyin, definitions]) => {
+        const pinyinTemplates = renderPinyin(pinyin);
+        const definitionTemplates = renderDefinitions(definitions);
+        itemTemplates.push(
+            html`<li class="chineselearningextension-definition-item">
+                ${pinyinTemplates}
+                ${definitionTemplates}
+            </li>`);
+    });
+    const template = html`<div>
+            <h2 class="chineselearningextension-popover-header">${word}</h2>
+        </div>
+        <ul class="chineselearningextension-definitions">${itemTemplates}</ul>`;
+    render(template, container);
+}
+
+// TODO: either add more fix-up logic (e.g., manually split 日出生 into [日,出生], or find other tokenizer)
+function allHaveTones(word) {
+    for (const character of word) {
+        if (!pinyin[character]) {
+            return false;
+        }
+    }
+    return true;
+}
+function overrideTones(word) {
+    let result = '';
+    for (const character of word) {
+        result += pinyin[character];
+    }
+    return result;
+}
+
+let pendingHideTimeout;
+function triggerHidePopover() {
+    if (pendingHideTimeout) {
+        return;
+    }
+    pendingHideTimeout = setTimeout(function () {
+        popover.hidePopover();
+    }, 500);
+}
+
+function cancelHidePopover() {
+    clearTimeout(pendingHideTimeout);
+    pendingHideTimeout = null;
+}
+
+popover.addEventListener('mouseenter', function () {
+    cancelHidePopover();
+});
+popover.addEventListener('mouseleave', function () {
+    triggerHidePopover();
+});
 
 //need iframe handling
 // to get background color: window.getComputedStyle(mod.node.parentElement).backgroundColor
@@ -32,34 +141,41 @@ modifications.forEach(mod => {
     const segments = segmenter.segment(fullText);
     // TODO: syntactic sugar
     Array.from(segments).forEach(segment => {
-        const tones = pinyin[segment.segment];
+        let tones = pinyin[segment.segment];
+        if (!tones && allHaveTones(segment.segment)) {
+            tones = overrideTones(segment.segment);
+        }
+        const wrapper = document.createElement('span');
         if (tones) {
             let i = 0;
-            const wrapper = document.createElement('span');
+            // TODO: use lit templates
             for (const character of segment.segment) {
                 const coloredSpan = document.createElement('span');
                 coloredSpan.innerText = character;
-                coloredSpan.addEventListener('mouseover', async function () {
-                    const response = await chrome.runtime.sendMessage({ word: segment.segment });
-                    if (!response.definitions) {
-                        return;
-                    }
-                    response.definitions.forEach(definition => {
-                        const definitionDiv = document.createElement('div');
-                        definitionDiv.innerHTML = `${definition.pinyin}: ${definition.en}`;
-                        popover.appendChild(definitionDiv);
-                    });
-                    popover.showPopover();
-                });
-                coloredSpan.addEventListener('mouseout', function () {
-                    popover.innerHTML = '';
-                    popover.hidePopover();
-                });
                 const tone = tones[i];
-                coloredSpan.style.color = tone === '1' ? '#eb3434' : tone === '2' ? 'green' : tone === '3' ? '#8f34eb' : tone === '4' ? '#345eeb' : '#333';
+                coloredSpan.style.color = getToneColor(tone);
                 wrapper.appendChild(coloredSpan);
                 i++;
             }
+            wrapper.addEventListener('mouseenter', async function () {
+                const response = await chrome.runtime.sendMessage({ word: segment.segment });
+                if (!response.definitions) {
+                    return;
+                }
+                cancelHidePopover();
+                const oldAnchors = document.getElementsByClassName('chineselearningextension-definition-anchor');
+                Array.from(oldAnchors).forEach(element => element.classList.remove('chineselearningextension-definition-anchor'));
+                wrapper.classList.add('chineselearningextension-definition-anchor');
+                const parsedDefinitions = parseDefinitions(response.definitions);
+                renderDictionary(segment.segment, parsedDefinitions, popover);
+                popover.showPopover();
+            });
+            wrapper.addEventListener('mouseleave', function () {
+                triggerHidePopover();
+            });
+            mod.parent.insertBefore(wrapper, mod.nextSibling);
+        } else {
+            wrapper.innerText = segment.segment;
             mod.parent.insertBefore(wrapper, mod.nextSibling);
         }
     });
