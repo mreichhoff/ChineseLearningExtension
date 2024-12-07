a8b1c300bb5b90aa12f7c31e2010c3212af5d87a
Matthew Reichhoff <matthew.reichhoff@gmail.com>
2024-12-07 11:44:35 -0500

    Switch to explicit ordering of external links

--- js/links.js
+++ js/links.js
@@ -1,34 +1,39 @@
 import { html } from 'lit-html';
 
-const linkSources = {
-    Tatoeba: {
-        linkTemplate: word => `https://tatoeba.org/eng/sentences/search?from=cmn&to=eng&query=${word}`,
-        description: 'sentences'
-    },
-    HanziGraph: {
-        linkTemplate: word => `https://hanzigraph.com/simplified/${word}`,
-        description: 'data'
-    },
-    Wiktionary: {
-        linkTemplate: word => `https://en.wiktionary.org/wiki/${word}`,
-        description: 'definitions'
-    },
-    Youglish: {
+const linkSources = [
+    {
+        name: 'Youglish',
         linkTemplate: word => `https://youglish.com/pronounce/${word}/chinese`,
         description: 'videos'
     },
-    Forvo: {
+    {
+        name: 'Forvo',
         linkTemplate: word => `https://forvo.com/search/${word}/zh/`,
         description: 'pronunciation'
-    }
-}
+    },
+    {
+        name: 'HanziGraph',
+        linkTemplate: word => `https://hanzigraph.com/simplified/${word}`,
+        description: 'data'
+    },
+    {
+        name: 'Wiktionary',
+        linkTemplate: word => `https://en.wiktionary.org/wiki/${word}`,
+        description: 'definitions'
+    },
+    {
+        name: 'Tatoeba',
+        linkTemplate: word => `https://tatoeba.org/eng/sentences/search?from=cmn&to=eng&query=${word}`,
+        description: 'sentences'
+    },
+];
 
 function renderExternalLinks(word) {
     return html`<h3>External links for ${word}</h3>
-    <ul class="chineselearningextension-link-list">${Object.entries(linkSources).map(([linkName, linkData]) => {
+    <ul class="chineselearningextension-link-list">${linkSources.map(linkData => {
         return html`<li class="chineselearningextension-link-item">
         Get <span class="chineselearningextension-emphasized">${linkData.description}</span> on
-        <a target="_blank" class="chineselearningextension-external-link" href="${linkData.linkTemplate(encodeURIComponent(word))}">${linkName}</a></li>`
+        <a target="_blank" class="chineselearningextension-external-link" href="${linkData.linkTemplate(encodeURIComponent(word))}">${linkData.name}</a></li>`
     })}</ul>`;
 }
 
